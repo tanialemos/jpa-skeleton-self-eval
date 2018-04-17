92c7762849a99fc29d5ee8ee405bac8793798951
tanialemos <tanialemosribeiro@gmail.com>
2018-04-17 16:31:51 +0200

    Division class done

--- src/main/java/be/niels/jpaskeleton/domain/division/Division.java
+++ src/main/java/be/niels/jpaskeleton/domain/division/Division.java
@@ -1,76 +1,77 @@
-//package be.niels.jpaskeleton.domain.division;
-//
-//import javax.persistence.Column;
-//import javax.persistence.Entity;
-//import javax.persistence.Id;
-//import javax.persistence.Table;
-//
-//@Entity
-//@Table(name = "DIVISIONS")
-//public class Division {
-//
-//    @Id
-//    @Column(name = "DIVISION_ID")
-//    private int divisionId;
-//    @Column(name = "DIVISION_ID")
-//    private String name;
-//    private String originalName;
-//    private String director;
-//
-//    public Division() {
-//    }
-//
-//    private Division(int divisionId, String name, String originalName, String director) {
-//        this.divisionId = divisionId;
-//        this.name = name;
-//        this.originalName = originalName;
-//        this.director = director;
-//    }
-//
-//    public int getDivisionId() {
-//        return divisionId;
-//    }
-//
-//    public String getName() {
-//        return name;
-//    }
-//
-//    public String getOriginalName() {
-//        return originalName;
-//    }
-//
-//    public String getDirector() {
-//        return director;
-//    }
-//
-//    public static class DivsionBuilder{
-//        private int divisionId;
-//        private String name;
-//        private String originalName;
-//        private String director;
-//
-//        public static DivsionBuilder divsionBuilder(){
-//            return new DivsionBuilder();
-//        }
-//
-//        private int withDivisionId(int divisionId){
-//            this.divisionId = divisionId;
-//        }
-//
-//        public void withName(String name) {
-//            this.name = name;
-//        }
-//
-//        public void withOriginalName(String originalName) {
-//            this.originalName = originalName;
-//        }
-//
-//        public void withDirector(String director) {
-//            this.director = director;
-//        }
-//
-//        public Division createDivision(){
-//            return new Division(divisionId, name, originalName, director);
-//        }
-//    }
-//}
+package be.niels.jpaskeleton.domain.division;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "DIVISIONS")
+public class Division {
+
+    @Id
+    @Column(name = "DIVISION_ID")
+    @SequenceGenerator(name = "division_seq", sequenceName = "division_seq", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "division_seq")
+    private int divisionId;
+    @Column(name = "NAME")
+    private String name;
+    @Column(name = "ORIGINALNAME")
+    private String originalName;
+    @Column(name = "DIRECTOR")
+    private String director;
+
+    public Division() {
+    }
+
+    private Division(int divisionId, String name, String originalName, String director) {
+        this.divisionId = divisionId;
+        this.name = name;
+        this.originalName = originalName;
+        this.director = director;
+    }
+
+    public int getDivisionId() {
+        return divisionId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getOriginalName() {
+        return originalName;
+    }
+
+    public String getDirector() {
+        return director;
+    }
+
+    public static class DivisionBuilder {
+        private int divisionId;
+        private String name;
+        private String originalName;
+        private String director;
+
+        public static DivisionBuilder divsionBuilder(){
+            return new DivisionBuilder();
+        }
+
+        public void withDivisionId(int divisionId){
+            this.divisionId = divisionId;
+        }
+
+        public void withName(String name) {
+            this.name = name;
+        }
+
+        public void withOriginalName(String originalName) {
+            this.originalName = originalName;
+        }
+
+        public void withDirector(String director) {
+            this.director = director;
+        }
+
+        public Division createDivision(){
+            return new Division(divisionId, name, originalName, director);
+        }
+    }
+}
